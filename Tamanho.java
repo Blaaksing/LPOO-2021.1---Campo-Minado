7122bd4726841a1725f7ca5ced8901b64b5b734a
Yan Monteiro de Melo <yandemelo@yahoo.com.br>
2021-11-21 22:12:22 -0300

    Delete Tamanho.java

--- Tamanho.java
+++
@@ -1,7 +0,0 @@
-package campominado;
-
-public class Tamanho {      //tamanho da matriz e a quantidade de minas
-    static int linhas = 9;
-    static int colunas = 9;
-    static int numminas = 10;
-}
