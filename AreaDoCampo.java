9e779d09faf14e496be85f5c47c0d1d5f495e77c
Blaaksing <88542990+Blaaksing@users.noreply.github.com>
2021-11-21 21:53:17 -0300

    Método de adicionar as minas
    Gera posições aleatórias para as minas, tomando como base o tamanho da matriz e a quantidade de bombas desejadas.

--- AreaDoCampo.java
+++ AreaDoCampo.java
@@ -1,4 +1,5 @@
 package campominado;
+import java.util.Random;
 
 public class AreaDoCampo {
     Quadrado[][] area;
@@ -10,9 +11,9 @@ public class AreaDoCampo {
                 area[i][j] = new Quadrado();
             }
         }
-    for (int i = 0; i < Tamanho.linhas; i++) {
+        for (int i = 0; i < Tamanho.linhas; i++) {
             for (int j = 0; j < Tamanho.colunas; j++) {
-                if (i > 0){                                     //condições para as bordas da area, para reconhecer as partes que não possuem quadrados
+                if (i > 0){                                     //condições para as bordas da area, onde não possui quadrado
                     if (j > 0) {
                         area[i][j].criarQuadradosProximos(area[i-1][j-1]);
                     }
@@ -37,9 +38,17 @@ public class AreaDoCampo {
                     }
                 }
             }
-        }
-        
-       }
+        } 
     }
-
-    
+    public void adicionarMinas(){           //adicionar as minas aleatoriamente na matriz
+        int n = Tamanho.numminas;
+        Random rand = new Random();
+        while (n > 0){            
+            int l = rand.nextInt(Tamanho.linhas); 
+            int c = rand.nextInt(Tamanho.colunas);             
+            if (area[l][c].possuiBomba()){
+                n--;
+            }            
+        }
+    }
+}
