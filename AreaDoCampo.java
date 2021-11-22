c1d88f392ee413af5bca4b45d9a563149482a468
Yan Monteiro de Melo <yandemelo@yahoo.com.br>
2021-11-21 21:48:24 -0300

    Condições
    Implementação das condições para a criação correta das bordas, para que reconheça os espaços que não terão quadrados em volta.

--- AreaDoCampo.java
+++ AreaDoCampo.java
@@ -10,5 +10,36 @@ public class AreaDoCampo {
                 area[i][j] = new Quadrado();
             }
         }
+    for (int i = 0; i < Tamanho.linhas; i++) {
+            for (int j = 0; j < Tamanho.colunas; j++) {
+                if (i > 0){                                     //condições para as bordas da area, para reconhecer as partes que não possuem quadrados
+                    if (j > 0) {
+                        area[i][j].criarQuadradosProximos(area[i-1][j-1]);
+                    }
+                        area[i][j].criarQuadradosProximos(area[i-1][j]);
+                    if (j < Tamanho.colunas-1) {
+                        area[i][j].criarQuadradosProximos(area[i-1][j+1]);
+                    }
+                    }
+                if (j > 0) {
+                    area[i][j].criarQuadradosProximos(area[i][j-1]);
+                }                
+                if (j < Tamanho.colunas-1){
+                    area[i][j].criarQuadradosProximos(area[i][j+1]);
+                }
+                if(i < Tamanho.linhas -1){
+                    if (j > 0){
+                        area[i][j].criarQuadradosProximos(area[i+1][j-1]);
+                    }
+                        area[i][j].criarQuadradosProximos(area[i+1][j]);
+                    if (j < Tamanho.colunas-1){
+                        area[i][j].criarQuadradosProximos(area[i+1][j+1]);
+                    }
+                }
+            }
+        }
+        
+       }
     }
-}
+
+    
