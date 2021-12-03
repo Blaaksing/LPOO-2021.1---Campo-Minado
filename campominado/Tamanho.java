94c5a26b2c4c91e44ddd045f1ba0d54767068c9e
Yan Monteiro de Melo <yandemelo@yahoo.com.br>
2021-12-03 00:19:49 -0300

    Atualização de dados
    Adição das variáveis static, que irão definir o tamanho da interface, e toda sua construção.

--- campominado/Tamanho.java
+++ campominado/Tamanho.java
@@ -1,7 +1,12 @@
+
 package campominado;
 
-public class Tamanho {      //tamanho da matriz e a quantidade de minas
-    static int linhas = 9;
-    static int colunas = 9;
-    static int numminas = 10;
+
+public abstract class Tamanho {             //tamanho da matriz e a quantidade de minas, juntamente com os tamanhos da interface
+    static int linhas = 6;
+    static int colunas = 6;
+    static int minas = 6;
+    static int espaco = 50;
+    static int margemSuperior = 100;
+    static int hBarra = 20;
 }
