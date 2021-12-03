39a7c98c6ea152e8d4b8389582fc1545cb256d53
Yan Monteiro de Melo <yandemelo@yahoo.com.br>
2021-12-03 00:10:52 -0300

    Atualização da Main
    Mudança do nome para maior entendimento, e adição das formas de testar o jogo.

---
+++ campominado/CampoMinadoMain.java
@@ -0,0 +1,15 @@
+
+package campominado;
+
+
+public class CampoMinadoMain {
+
+    public static void main(String[] args) { //apenas para testar o jogo
+        JFrameCampo i = new JFrameCampo();
+    }
+    
+    public static void hardReset(){
+        JFrameCampo i = new JFrameCampo();
+    }
+    
+}
