1944b5bea2d262556db548f84dc378e6e7e4738b
Blaaksing <88542990+Blaaksing@users.noreply.github.com>
2021-12-07 13:58:31 -0300

    Campo refatorado
    Esquecemos de refatorar isso junto com as 'múltiplas implementações' - Tamanho padrão do campo refatorado para 5x5 e 6 minas.

--- campominado/Tamanho.java
+++ campominado/Tamanho.java
@@ -1,8 +1,8 @@
 package campominado;
 
 public abstract class Tamanho {             //tamanho da matriz e a quantidade de minas, juntamente com os tamanhos da interface
-   public static int linhas = 6;
-   public static int colunas = 6;
+   public static int linhas = 5;
+   public static int colunas = 5;
    public static int minas = 6;
    public static int espaco = 50;
    public static int margemSuperior = 100;
