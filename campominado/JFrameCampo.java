b08f65a635dbb4efa794d7536dd9c7e9cbdb257d
Blaaksing <88542990+Blaaksing@users.noreply.github.com>
2021-12-16 02:03:02 -0300

    Multíplas Implementações
    Adicionadas imagens para representar a dificuldade do jogo e iniciação da implementação da interface

--- campominado/JFrameCampo.java
+++ campominado/JFrameCampo.java
@@ -1,15 +1,14 @@
 package campominado;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import exceptions.*;
 
+public class JFrameCampo extends JFrame implements Interface{
 
-public class JFrameCampo extends JFrame {
-
-   
     JPanel panel;
     JButtonQuadrado[][] quadrado;
     AreaDoCampo c;
@@ -18,22 +17,25 @@ public class JFrameCampo extends JFrame {
     JButton medBut;
     JButton difBut;
     JButton custBut;
- // JButton recordeBut;
+    JButton rankingBut;
 
-    public JFrameCampo() {
+    public JFrameCampo(){
     	System.out.println();
         confIniciais();
     }
 
     public void hardReset() {
+
         CampoMinadoMain.hardReset();
+        JButtonQuadrado.contMarc=0;
         this.dispose();
         
     }
      
     
-    private void confIniciais() {
-        
+    public void confIniciais(){
+        campominado.Temporizador.timer.start();
+        Temporizador reset = new Temporizador();
         
         //fim do temporizador
         this.c = new AreaDoCampo();
@@ -58,95 +60,101 @@ public class JFrameCampo extends JFrame {
         }
         //tamanho da interface
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(Tamanho.colunas * Tamanho.espaco+15, Tamanho.linhas * Tamanho.espaco + Tamanho.margemSuperior + Tamanho.hBarra +17);
-        this.setResizable(true);
+        this.setSize(Tamanho.colunas * Tamanho.espaco+15, Tamanho.linhas * Tamanho.espaco + Tamanho.margemSuperior + Tamanho.hBarra +80);
+        this.setResizable(false);
         this.setVisible(true);
         
-
-      /*  PROTOTIPO MUITO INICIAL DE MÉTODO PARA ARMAZENAR OS RECORDES
-       
-        this.recordeBut = new JButton("Recordes");
-       
-        this.recordeBut.addActionListener((java.awt.event.ActionEvent evt) -> {
-        	
-        	this.panel.add();
-        });
+        //ranking
+        //Icon icon = new ImageIcon("");
+		this.rankingBut = new JButton("Ranking");
+		this.rankingBut.addActionListener((java.awt.event.ActionEvent evt) -> {
+			Ranking save2 = new Ranking();
+			save2.lerLista();
+		
+		});
+		this.rankingBut.setSize(Tamanho.espaco+40, Tamanho.espaco);
+        this.rankingBut.setLocation((Tamanho.espaco * Tamanho.colunas) / 2 - Tamanho.espaco / 2 - 20, (Tamanho.espaco * Tamanho.linhas)+105);
+        this.panel.add(this.rankingBut);
+        //
         
-       */ 
-        
-        this.resetBut = new JButton("Reiniciar");
+        this.resetBut = new JButton("");
         this.resetBut.addActionListener((java.awt.event.ActionEvent evt) -> {
             
+            reset.reset();
             hardReset();
             this.reiniciar();
         });
         this.resetBut.setSize(Tamanho.espaco+40, Tamanho.espaco);
         this.resetBut.setLocation((Tamanho.espaco * Tamanho.colunas) / 2 - Tamanho.espaco / 2 - 20, Tamanho.margemSuperior - Tamanho.espaco);
         this.panel.add(this.resetBut);
+        resetBut.setIcon(new ImageIcon(getClass().getResource("galinha.png")));
 
 
-        this.facilBut = new JButton("F");												// Campo no modo fácil
+        this.facilBut = new JButton("");												// Campo no modo fácil
         this.facilBut.addActionListener((java.awt.event.ActionEvent evt) -> {
             Tamanho.colunas = 5;
             Tamanho.linhas = 5;
             Tamanho.minas = 6;
             JButtonQuadrado.contMarc = 0;
-        //    Temporizador.seconds = 0;					// O temporizador reinicia para o zero, mas ainda não consegui implementar um meio dele iniciar a contagem.
+            reset.reset();
             this.hardReset();
         });
         this.facilBut.setSize((Tamanho.espaco * Tamanho.colunas) / 4, Tamanho.espaco);
         this.facilBut.setLocation(0, 0);
         this.panel.add(this.facilBut);
-
+        facilBut.setIcon(new ImageIcon(getClass().getResource("zumbi.png")));
         
-        this.medBut = new JButton("M");													// Campo no modo médio
+        
+        this.medBut = new JButton("");													// Campo no modo médio
         this.medBut.addActionListener((java.awt.event.ActionEvent evt) -> {
             Tamanho.colunas = 8;
             Tamanho.linhas = 10;
             Tamanho.minas = 20;
             JButtonQuadrado.contMarc = 0;
-         //   Temporizador.seconds = 0;					// O temporizador reinicia para o zero, mas ainda não consegui implementar um meio dele iniciar a contagem.
+            reset.reset();
             this.hardReset();
         });
         this.medBut.setSize((Tamanho.espaco * Tamanho.colunas) / 4, Tamanho.espaco);
         this.medBut.setLocation((Tamanho.espaco * Tamanho.colunas) / 4, 0);
         this.panel.add(this.medBut);
+        medBut.setIcon(new ImageIcon(getClass().getResource("esqueleto.png")));
 
 
-        this.difBut = new JButton("D");													// Campo no modo difícil
+        this.difBut = new JButton("");													// Campo no modo difícil
         this.difBut.addActionListener((java.awt.event.ActionEvent evt) -> {
             Tamanho.colunas = 14;
             Tamanho.linhas = 12;
             Tamanho.minas = 44;
             JButtonQuadrado.contMarc = 0;
-         //   Temporizador.seconds = 0;					// O temporizador reinicia para o zero, mas ainda não consegui implementar um meio dele iniciar a contagem.
+            reset.reset();
             this.hardReset();
         });
         this.difBut.setSize((Tamanho.espaco * Tamanho.colunas) / 4, Tamanho.espaco);
         this.difBut.setLocation((Tamanho.espaco * Tamanho.colunas) / 4 * 2, 0);
         this.panel.add(this.difBut);
+        difBut.setIcon(new ImageIcon(getClass().getResource("creeper.png")));
       
         
-        this.custBut = new JButton("C");
+
+        this.custBut = new JButton("");
         this.custBut.addActionListener((java.awt.event.ActionEvent evt) -> {            				//	Iniciar o campo customizado
-            int l = Integer.parseInt(JOptionPane.showInputDialog("Quantas linhas? \n Máximo: 14"));
+            int l = Integer.parseInt(JOptionPane.showInputDialog("Quantas linhas? \n Maximo: 14"));
             Tamanho.linhas = l;
               
-            int c = Integer.parseInt(JOptionPane.showInputDialog("Quantas colunas? \n Máximo: 30"));
+            int c = Integer.parseInt(JOptionPane.showInputDialog("Quantas colunas? \n Maximo: 30"));
             Tamanho.colunas = c;
 
-            int m = Integer.parseInt(JOptionPane.showInputDialog("Quantas minas? \n Máximo: "+(m=(l*c)-1)));
+            int m = Integer.parseInt(JOptionPane.showInputDialog("Quantas minas? \n Maximo: "+(m=(l*c)-1)));
             Tamanho.minas = m;
 
             if (Tamanho.linhas > 14 || Tamanho.colunas > 30 || Tamanho.minas > (m=(l*c)-1)) {
                 try {
                 	
-                	throw new LimiteLinhaColunaMinasExcedido(Tamanho.linhas, Tamanho.colunas, Tamanho.minas); //Exceção para caso, no tabuleiro customizado, o tamanho, a coluna ou as minas ultrapassem o limite permitido
+                	throw new LimiteLinhaColunaMinasExcedido(Tamanho.linhas, Tamanho.colunas, Tamanho.minas); //Excecao para caso, no tabuleiro customizado, o tamanho, a coluna ou as minas ultrapassem o limite permitido
                 	}
                 	catch(ArrayIndexOutOfBoundsException | LimiteLinhaColunaMinasExcedido e) {
-                		JOptionPane.showMessageDialog(null,"Algum parâmetro foi passado de maneira equivocada.\nTente novamente");
-                		System.err.println("ALGUM PARÂMETRO FOI PASSADO DE MANEIRA ERRÔNEA. \nTENTE NOVAMENTE!");
-                        //voltar para o modo fácil (padrão)
+                		JOptionPane.showMessageDialog(null,"Algum parametro foi passado de maneira equivocada.\nTente novamente");
+                        //voltar para o modo f�cil (padr�o)
                         Tamanho.linhas = 5;
                         Tamanho.colunas = 5;
                         Tamanho.minas = 5;
@@ -157,15 +165,18 @@ public class JFrameCampo extends JFrame {
                 	}
                 }
             
-            
             JButtonQuadrado.contMarc=0;
-         
+            reset.reset();
             this.hardReset();
             
         });
         this.custBut.setSize((Tamanho.espaco * Tamanho.colunas) / 4, Tamanho.espaco);
         this.custBut.setLocation((Tamanho.espaco * Tamanho.colunas) / 4 * 3, 0);
         this.panel.add(this.custBut);
+        custBut.setIcon(new ImageIcon(getClass().getResource("enderman.png")));
+
+        
+        
 
     }
 
@@ -197,19 +208,49 @@ public class JFrameCampo extends JFrame {
     }
 
     public void checkEstado() {
-        System.out.println("Verificando se Ganhou ou Perdeu");
         if (this.c.vitoria()) {
+            Ranking save1 = new Ranking();
+            Temporizador reset = new Temporizador();
+			campominado.Temporizador.timer.stop();
+            JOptionPane.showMessageDialog(null,"VOCE VENCEU!! :D");
+            save1.salvar();
+            reset.reset();
             this.desativaBotoes();
-            JOptionPane.showMessageDialog(null,"VOCÊ VENCEU!! :D");
             hardReset();
         }
 
         if (this.c.derrota()) {
-        	this.desativaBotoes();
-        	JOptionPane.showMessageDialog(null,"Você perdeu!! :(");
+            
+			campominado.Temporizador.timer.stop();
+            Temporizador reset1 = new Temporizador();
+            
+            reset1.reset();
+            
+        	//this.desativaBotoes();
+        	JOptionPane.showMessageDialog(null,"Voce perdeu!! :(");
             hardReset();
+            
+            
 
         }
     }
+
+	@Override
+	public void marcar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void revela(String cod) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPos(int linhas, int colunas) {
+		// TODO Auto-generated method stub
+		
+	}
    
 }
