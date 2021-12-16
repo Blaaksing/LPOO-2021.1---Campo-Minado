7fd728bf597f1d392947c77b8e1fd68ad5fd2465
Yan Monteiro de Melo <yandemelo@yahoo.com.br>
2021-12-16 01:49:22 -0300

    Remoção do temporizador e comentário da interface
    Adição dos significados na hora dos gráficos e remoção do temporizador, para criá-lo em outra classe

--- campominado/CampoMinadoMain.java
+++ campominado/CampoMinadoMain.java
@@ -1,63 +1,40 @@
 package campominado;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class CampoMinadoMain {
-    //metodo para implementar o temporizador
-    private JLabel interfaceTempo;
-    private Timer timer;
-    private int contador = 0;
-
-
-    public void init(JPanel cronometro){
-        
-        interfaceTempo = new JLabel("00:00");
-        Dimension size = interfaceTempo.getPreferredSize();
-        interfaceTempo.setBounds(10, 65, size.width, size.height);
-        cronometro.add(interfaceTempo, BorderLayout.CENTER);
-        
-            timer = new Timer();
-            timer.scheduleAtFixedRate(new TimerTask(){
-                @Override
-                public void run(){
-                    contador++;
-                    int segundos = contador % 60;
-                    int minutos = contador / 60;
-                    minutos %= 60;
-                    interfaceTempo.setText(String.format("%02d:%02d", minutos , segundos));
-                }
-            }, 1000, 1000);
-            
-        
-    }
+    
+    JPanel panel;
     public static void main(String[] args) { 
-        hardReset(); //iniciar do zero, para os frames serem resetados, juntamente com o tempo
+        JFrameCampo campo = new JFrameCampo();	 //iniciar do zero, para os frames serem resetados, juntamente com o tempo
+        Temporizador Temporizador = new Temporizador(campo);
     }
 
     public static void hardReset(){
         
-    	
-        JFrameCampo i = new JFrameCampo();	
-        CampoMinadoMain tempo = new CampoMinadoMain();
-        EventQueue.invokeLater( new Runnable() {
-            
-             @Override
-             public void run() {
-                 tempo.init(i.panel);
-                 
-                }
-                
-            }
-            );
+        JFrameCampo campo = new JFrameCampo();	
+        Temporizador Temporizador = new Temporizador(campo);
             
         }
     }
     
+    /*
+    Proximidade:
+    0 - pedra
+    1 - carvao
+    2 - ferro
+    3 - ouro
+    4 - diamante
+    5 - esmeralda
+    6 - lapislazuli
+    7 - redstone
+    8 - ametista
     
+    Dificuldade:
+    Facil: Zumbi
+    Medio: Esqueleto
+    Dificil: Creeper
+    Customizado: Enderman
+    
+    Botão reiniciar: Galinha
+    */
